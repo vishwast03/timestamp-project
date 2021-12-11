1e8bb9cd95031eebeb8d632d1ee0ea644d782baf
vishwast03 <>
2021-12-11 14:33:41 +0000

    fix bug

--- server.js
+++ server.js
@@ -74,44 +74,7 @@ app.get("/api/:date?", function (req, res) {
 
 });
 
-// app.get("/api/:date?", function (req, res) {
-
-//   let date;
-//   let regexp = /^[0-9]+$/;
-//   if (req.params.date) {
-//     if (req.params.date.match(regexp))
-//       date = new Date(parseInt(req.params.date));
-//     else
-//       date = new Date(req.params.date);
-//   }
-//   else {
-//     date = new Date();
-//   }
-
-//   if (isNaN(date.getTime())) {
-//     res.json({ error: "Invalid Date" });
-//   }
-//   else {
-//     const unixDate = date.getTime();
-
-//     const utcDay = `${weekDays[date.getUTCDay()]}`;
-//     const utcDate = `${date.getUTCDate()}`;
-//     const utcMonth = `${months[date.getUTCMonth()]}`;
-//     const utcYear = `${date.getUTCFullYear()}`;
-//     const utcHour = `${date.getUTCHours() === 0 ? "00" : date.getUTCHours()}`;
-//     const utcMinute = `${date.getUTCMinutes() === 0 ? "00" : date.getUTCMinutes()}`;
-//     const utcSecond = `${date.getUTCSeconds() === 0 ? "00" : date.getUTCSeconds()}`;
-
-//     const utcDateString = `${utcDay}, ${utcDate} ${utcMonth} ${utcYear} ${utcHour}:${utcMinute}:${utcSecond} GMT`;
-
-//     res.json({ unix: unixDate, utc: utcDateString });
-//   }
-// });
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-// var listener = app.listen(3000, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
